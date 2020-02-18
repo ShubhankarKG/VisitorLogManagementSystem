23f911557dcee323145d954b609e001b7888a9aa
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-02-18 21:41:12 +0530

    Changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*esversion : 6*/
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -8,7 +9,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('A user connected');
-})
+});
 
 http.listen(3000, () => {
     console.log("Listening on port *:3000");
