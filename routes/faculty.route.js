b44d29d9ce96c588f7cab8a4437b0ab04a996e9a
HarsimranVirk <harsimranvirk99@gmail.com>
2020-04-27 21:09:34 +0530

    Added build with client-side routing and npm start script

--- routes/faculty.route.js
+++ routes/faculty.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const faculty_controller = require("../controllers/faculty.controller");
 
-router.get("/", faculty_controller.faculty_retrieve);
+router.get("/", auth, faculty_controller.faculty_retrieve);
 router.post("/create",auth, faculty_controller.faculty_create);
 
 module.exports = router;
