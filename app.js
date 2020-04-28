fada1fcfd429ed75f3befc55160c1d2e2215cdf0
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-04-28 17:34:37 +0530

    Heroku deployment

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
 const path = require('path');
 
 const visitor = require('./routes/visitor.route');
@@ -20,7 +19,6 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(morgan("combined"));
 
 app.use('/api/visitor', visitor);
 app.use('/api/faculty', faculty);
