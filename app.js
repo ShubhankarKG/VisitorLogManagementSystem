51cb2f6ca5a674f4fdb9d91e180746f6c0abcae5
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-04-28 17:03:03 +0530

    Heroku deployment

---
+++ app.js
@@ -0,0 +1,39 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const visitor = require('./routes/visitor.route');
+const faculty = require('./routes/faculty.route');
+const visitingDetails = require('./routes/visitingDetails.route');
+const admin = require('./routes/admin.route');
+
+const mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("DB Connected"))
+    .catch((error) => console.log(error));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB Connection Error:'));
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(morgan("combined"));
+
+app.use('/api/visitor', visitor);
+app.use('/api/faculty', faculty);
+app.use('/api/dashboard', visitingDetails);
+app.use('/api/admin', admin);
+
+app.set('view engine', 'ejs');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, "client/build/index.html"));
+    });
+}
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,51 +1,7 @@
 /*esversion : 6*/
 require('dotenv').config();
-const https = require('https');
-const fs = require('fs');
-const cors = require('cors');
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const path = require('path');
+const app = require("./app");
 
-const options = {
-    key: fs.readFileSync("rootSSL.key"),
-    cert: fs.readFileSync("rootSSL.pem"),
-    passphrase: process.env.FILE_PASSPHRASE
-};
+const port = process.env.PORT || 5000;
 
-const visitor = require('./routes/visitor.route');
-const faculty = require('./routes/faculty.route');
-const visitingDetails = require('./routes/visitingDetails.route');
-const admin = require('./routes/admin.route');
-
-const mongoDB = process.env.MONGODB_URL;
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log("DB Connected"))
-.catch((error) => console.log(error));
-
-const db = mongoose.connection;
-const app = express();
-app.use(cors());
-app.use(express.json({ limit : '50mb' }));
-app.use(express.urlencoded({ extended : true, limit : '50mb' }));
-app.use(morgan("combined"));
-
-app.use('/api/visitor', visitor);
-app.use('/api/faculty', faculty);
-app.use('/api/dashboard', visitingDetails);
-app.use('/api/admin', admin);
-app.use(express.static('client/build'));
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
-
-app.set('view engine', 'ejs');
-db.on('error', console.error.bind(console, 'MongoDB Connection Error:'));
-
-var server = https.createServer(options, app);
-
-server.listen(5000, () => {
-    console.log("Listening on port *:5000");
-});
+app.listen(port, () => console.log(`Running on port ${port}`));
